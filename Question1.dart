9ce95f7303999fb7c4258084672a41a8fb6039ea
Siphamandla  Mate <matesiphamandla98@gmail.com>
2022-05-25 22:28:21 +0000

    Upload New File

---
+++ Question1.dart
@@ -0,0 +1,9 @@
+void main() {
+  var nameandsurname = "Siphamandla Mate";
+  var favouriteapp = "Soccer Star";
+  var city = "Bloemfontein";
+
+  print("My Name is $nameandsurname");
+  print("My Favourite app is $favouriteapp");
+  print("I'm currently situated in $city");
+}
